0e7f9a9845a02d62adf62af0241c1997e8846c57
steventhorne <steven.thorne91@gmail.com>
2022-08-27 15:07:03 -0400

    Update lsp plugins
    Migrate most servers to use lsp api instead of lspconfig.
    Update angularls/tsserver interactions to prevent duplicate
    functionality.
    Show lsp_signature virtual text hints.
    Add lsp util for root_dir.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,9 @@
 local function configure()
-  local lspconfig = require("lspconfig")
-  local util = require("lspconfig.util")
-
   local lsp_signature_opts = {
     floating_window = false,
-    floating_window_off_y = -2,
     close_timeout = 2000,
-    hint_enable = false,
+    hint_enable = true,
+    hint_prefix = "",
   }
   require("lsp_signature").setup(lsp_signature_opts)
 
@@ -15,9 +12,10 @@ local function configure()
       autoSetHints = true,
       -- hover_with_actions = true, -- https://github.com/simrat39/rust-tools.nvim#setup
       inlay_hints = {
+        auto = true,
         show_parameter_hints = false,
         parameter_hints_prefix = "",
-        other_hints_prefix = "",
+        other_hints_prefix = ": ",
       },
     },
     server = {
@@ -33,20 +31,16 @@ local function configure()
 
   require("rust-tools").setup(rust_tools_opts)
 
-  local capabilities = {
-    textDocument = {
-      completion = {
-        completionItem = {
-          snippetSupport = true,
-        }
-      }
-    }
-  }
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
+  local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+  default_capabilities.textDocument.completion.completionItem.snippetSupport = true
+  default_capabilities = require("cmp_nvim_lsp").update_capabilities(default_capabilities)
 
   local pid = vim.fn.getpid()
   local configPath = vim.fn.stdpath("config")
   local nodeModulesPath = configPath.."/node_modules"
+
+  local au_lsp = vim.api.nvim_create_augroup("LSP", { clear = true })
 
   local angularlsCmd = {
     "node",
@@ -59,70 +53,200 @@ local function configure()
     "--includeCompletionsWithSnippetText",
     "--includeAutomaticOptionalChainCompletions",
   }
-  lspconfig.angularls.setup({
-    cmd = angularlsCmd,
-    capabilities = capabilities,
-    root_dir = util.root_pattern("angular.json"),
-    on_new_config = function(new_config)
-      new_config.cmd = angularlsCmd
-    end
-  })
-
-  lspconfig.tsserver.setup({
-    capabilities = capabilities,
-    cmd = { "node", nodeModulesPath.."/typescript-language-server/lib/cli.js", "--stdio" },
-  })
-
-  lspconfig.html.setup({
-    capabilities = capabilities,
-    cmd = { "node", nodeModulesPath.."/vscode-langservers-extracted/bin/vscode-html-language-server", "--stdio" },
-  })
-
-  lspconfig.cssls.setup({
-    capabilities = capabilities,
-    cmd = { "node", nodeModulesPath.."/vscode-langservers-extracted/bin/vscode-css-language-server", "--stdio" },
-  })
-
-  lspconfig.svelte.setup({
-    capabilities = capabilities,
-    cmd = { "node", nodeModulesPath.."/svelte-language-server/bin/server.js", "--stdio" },
-  })
-
-  lspconfig.astro.setup({
-    capabilities = capabilities,
-    cmd = { "node", nodeModulesPath.."/@astrojs/language-server/bin/nodeServer.js", "--stdio" },
-    root_dir = util.root_pattern("package.json"),
-  })
-
-  lspconfig.omnisharp.setup({
-    capabilities = capabilities,
-    -- on_attach = function(_, bufnr)
-    --   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "cmp#omnifunc")
-    -- end,
-    cmd = { "E:\\Programs\\OmniSharp\\OmniSharp.exe", "--languageserver", "--hostPID", tostring(pid) },
-  })
-
-  lspconfig.sumneko_lua.setup({
-    capabilities = capabilities,
-    cmd = { "E:\\Programs\\LuaLanguageServer\\bin\\lua-language-server.exe" },
-    settings = {
-      Lua = {
-        runtime = {
-          version = "LuaJIT",
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
-          maxPreload = 2000,
-          preloadFileSize = 1000
-        },
-        telemetry = {
-          enabled = false,
-        },
-      },
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = {
+      "typescript",
+      "html",
+      "typescriptreact",
+      "typescript.tsx",
     },
+    callback = function()
+      if vim.fn.expand("%:e") == ".cshtml" then
+        return
+      end
+      local root_dir = require("utils").get_root_dir({ "angular.json" })
+      if root_dir ~= nil then
+        vim.b.angularls_enabled = true
+        vim.b.capabilities = default_capabilities
+        vim.lsp.start({
+          name = "angularls",
+          cmd = angularlsCmd,
+          capabilities = default_capabilities,
+          root_dir = root_dir,
+          on_new_config = function(new_config)
+            new_config.cmd = angularlsCmd
+          end,
+        })
+      end
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = {
+      "javascript",
+      "javascriptreact",
+      "javascript.jsx",
+      "typescript",
+      "typescriptreact",
+      "typescript.tsx",
+    },
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ "package.json", "tsconfig.json", "jsconfig.json" })
+      vim.lsp.start({
+        name = "tsserver",
+        cmd = { "node", nodeModulesPath.."/typescript-language-server/lib/cli.js", "--stdio" },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+        init_options = { hostInfo = "neovim" },
+        on_attach = function(client, _)
+          if vim.b.angularls_enabled then
+            client.server_capabilities.referencesProvider = false
+            client.server_capabilities.renameProvider = false
+          end
+        end
+      })
+    end,
+  })
+
+  -- lspconfig.html.setup({
+  --   capabilities = default_capabilities,
+  --   cmd = { "node", nodeModulesPath.."/vscode-langservers-extracted/bin/vscode-html-language-server", "--stdio" },
+  -- })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = "html",
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ "package.json", ".git" }, true)
+      vim.lsp.start({
+        name = "html",
+        cmd = { "node", nodeModulesPath.."/vscode-langservers-extracted/bin/vscode-html-language-server", "--stdio" },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+      })
+    end,
+  })
+
+  -- lspconfig.cssls.setup({
+  --   capabilities = default_capabilities,
+  --   cmd = { "node", nodeModulesPath.."/vscode-langservers-extracted/bin/vscode-css-language-server", "--stdio" },
+  -- })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = { "css", "less", "scss" },
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ "package.json", ".git" }, true)
+      vim.lsp.start({
+        name = "css",
+        cmd = { "node", nodeModulesPath.."/vscode-langservers-extracted/bin/vscode-css-language-server", "--stdio" },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+      })
+    end,
+  })
+
+  -- lspconfig.svelte.setup({
+  --   capabilities = default_capabilities,
+  --   cmd = { "node", nodeModulesPath.."/svelte-language-server/bin/server.js", "--stdio" },
+  -- })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = "svelte",
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ "package.json", ".git" })
+      vim.lsp.start({
+        name = "svelte",
+        cmd = { "node", nodeModulesPath.."/svelte-language-server/bin/server.js", "--stdio" },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+      })
+    end,
+  })
+
+  -- lspconfig.astro.setup({
+  --   capabilities = default_capabilities,
+  --   cmd = { "node", nodeModulesPath.."/@astrojs/language-server/bin/nodeServer.js", "--stdio" },
+  --   root_dir = util.root_pattern("package.json"),
+  -- })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = "astro",
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ "package.json", "tsconfig.json", "jsconfig.json", ".git" })
+      vim.lsp.start({
+        name = "astro",
+        cmd = { "node", nodeModulesPath.."/@astrojs/language-server/bin/nodeServer.js", "--stdio" },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+      })
+    end,
+  })
+
+  -- lspconfig.omnisharp.setup({
+  --   capabilities = default_capabilities,
+  --   cmd = { "E:\\Programs\\OmniSharp\\OmniSharp.exe", "--languageserver", "--hostPID", tostring(pid) },
+  -- })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = {"cs", "vb"},
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ "*.sln", "*.csproj" })
+      vim.lsp.start({
+        name = "omnisharp",
+        cmd = { "E:\\Programs\\OmniSharp\\OmniSharp.exe", "--languageserver", "--hostPID", tostring(pid) },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+      })
+    end,
+  })
+
+  -- lspconfig.sumneko_lua.setup({
+  --   capabilities = default_capabilities,
+  --   cmd = { "E:\\Programs\\LuaLanguageServer\\bin\\lua-language-server.exe" },
+  --   settings = {
+  --     Lua = {
+  --       runtime = {
+  --         version = "LuaJIT",
+  --       },
+  --       diagnostics = {
+  --         globals = { "vim" },
+  --       },
+  --       workspace = {
+  --         library = vim.api.nvim_get_runtime_file("", true),
+  --         maxPreload = 2000,
+  --         preloadFileSize = 1000
+  --       },
+  --       telemetry = {
+  --         enabled = false,
+  --       },
+  --     },
+  --   },
+  -- })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = au_lsp,
+    pattern = "lua",
+    callback = function()
+      local root_dir = require("utils").get_root_dir({ ".luarc.json", ".luacheckrc", "stylua.toml", ".git" })
+      vim.lsp.start({
+        name = "lua",
+        cmd = { "E:\\Programs\\LuaLanguageServer\\bin\\lua-language-server.exe" },
+        capabilities = default_capabilities,
+        root_dir = root_dir,
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT", },
+            diagnostics = { globals = { "vim" }, },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              maxPreload = 2000,
+              preloadFileSize = 1000
+            },
+            telemetry = { enabled = false, },
+          },
+        }
+      })
+    end,
   })
 
   vim.diagnostic.config({ severity_sort=true })
--- lua/utils.lua
+++ lua/utils.lua
@@ -11,7 +11,32 @@ local function map_key(mode, key, result, opts)
   )
 end
 
+local function get_root_dir(patterns, use_cwd_as_fallback)
+  use_cwd_as_fallback = use_cwd_as_fallback or false
+  local cwd = vim.fn.getcwd()
+  local bufdir = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+
+  while true do
+    for _, pattern in ipairs(patterns) do
+      for _, p in ipairs(vim.fn.glob(bufdir .. "/" .. pattern, true, true)) do
+        return bufdir
+      end
+    end
+    if bufdir == cwd then
+      break
+    end
+    bufdir = vim.fs.dirname(bufdir)
+  end
+
+  if use_cwd_as_fallback then
+    return cwd
+  end
+
+  return nil
+end
+
 return {
   map_key = map_key,
+  get_root_dir = get_root_dir,
 }
 
