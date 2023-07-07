04cb03e5d6c827714141b59420d623f72be0a8e6
steventhorne <steven.thorne91@gmail.com>
2023-07-07 16:40:20 -0400

    Improve nvim-tree keymapping/settings

--- lua/sthorne/plugins/tree.lua
+++ lua/sthorne/plugins/tree.lua
@@ -6,9 +6,15 @@ local function configure()
       side = "right",
       number = true,
       relativenumber = true,
-    }
+    },
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      },
+    },
   })
   require("sthorne.utils").map_key("n", "<LEADER>nn", ":NvimTreeToggle<CR>")
+  require("sthorne.utils").map_key("n", "<LEADER>nN", ":NvimTreeFindFile<CR>")
 end
 
 return {
