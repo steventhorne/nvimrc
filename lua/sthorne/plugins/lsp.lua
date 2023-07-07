966a9c88c84cf3c7e5cc811e9da932e2f7fba804
steventhorne <steven.thorne91@gmail.com>
2023-07-07 16:40:00 -0400

    Add better longterm omnisharp token fix

--- lua/sthorne/plugins/lsp.lua
+++ lua/sthorne/plugins/lsp.lua
@@ -259,77 +259,25 @@ local function configure()
     end,
     on_attach = function(client, bufnr)
       vim.api.nvim_buf_set_keymap(bufnr, "n", "<LEADER>ld", ":lua require('omnisharp_extended').lsp_definitions()<CR>", { noremap = true, silent = true })
+
+      local function to_snake_case(str)
+        return string.gsub(str, "%s*[- ]%s*", "_")
+      end
+      -- fix tokenModifiers and tokenTypes that don't conform to LSP semantic tokens spec
+      -- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
+      local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+      for i, v in ipairs(tokenModifiers) do
+        tokenModifiers[i] = to_snake_case(v)
+      end
+      local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+      for i, v in ipairs(tokenTypes) do
+        tokenTypes[i] = to_snake_case(v)
+      end
       client.server_capabilities.semanticTokensProvider = {
         full = vim.empty_dict(),
         legend = {
-          tokenModifiers = { "static_symbol" },
-          tokenTypes = {
-            "comment",
-            "excluded_code",
-            "identifier",
-            "keyword",
-            "keyword_control",
-            "number",
-            "operator",
-            "operator_overloaded",
-            "preprocessor_keyword",
-            "string",
-            "whitespace",
-            "text",
-            "static_symbol",
-            "preprocessor_text",
-            "punctuation",
-            "string_verbatim",
-            "string_escape_character",
-            "class_name",
-            "delegate_name",
-            "enum_name",
-            "interface_name",
-            "module_name",
-            "struct_name",
-            "type_parameter_name",
-            "field_name",
-            "enum_member_name",
-            "constant_name",
-            "local_name",
-            "parameter_name",
-            "method_name",
-            "extension_method_name",
-            "property_name",
-            "event_name",
-            "namespace_name",
-            "label_name",
-            "xml_doc_comment_attribute_name",
-            "xml_doc_comment_attribute_quotes",
-            "xml_doc_comment_attribute_value",
-            "xml_doc_comment_cdata_section",
-            "xml_doc_comment_comment",
-            "xml_doc_comment_delimiter",
-            "xml_doc_comment_entity_reference",
-            "xml_doc_comment_name",
-            "xml_doc_comment_processing_instruction",
-            "xml_doc_comment_text",
-            "xml_literal_attribute_name",
-            "xml_literal_attribute_quotes",
-            "xml_literal_attribute_value",
-            "xml_literal_cdata_section",
-            "xml_literal_comment",
-            "xml_literal_delimiter",
-            "xml_literal_embedded_expression",
-            "xml_literal_entity_reference",
-            "xml_literal_name",
-            "xml_literal_processing_instruction",
-            "xml_literal_text",
-            "regex_comment",
-            "regex_character_class",
-            "regex_anchor",
-            "regex_quantifier",
-            "regex_grouping",
-            "regex_alternation",
-            "regex_text",
-            "regex_self_escaped_character",
-            "regex_other_escape",
-          },
+          tokenModifiers = tokenModifiers,
+          tokenTypes = tokenTypes,
         },
         range = true,
       }
