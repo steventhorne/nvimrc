3fa27d961b987b307c8bf6bda519c862a5e88b63
Steve Thorne <steven.thorne91@gmail.com>
2024-05-21 15:18:01 -0400

    Add settings to term

--- lua/sthorne/keymappings.lua
+++ lua/sthorne/keymappings.lua
@@ -83,6 +83,15 @@ vim.cmd([[
   endfunction
 ]])
 
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.scrolloff = 0
+  end,
+})
+
 vim.cmd([[
   function! RemoveQFItem()
     let curqfidx = line('.') - 1
