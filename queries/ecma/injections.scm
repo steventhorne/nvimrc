2fe92e19fd510409db6c130c1830b7ac08f013de
Steve Thorne <steven.thorne91@gmail.com>
2024-05-21 15:18:08 -0400

    Fix ecma/sql injection

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -1,4 +1,4 @@
 ; extends
-(((template_string) @_template_string
-  (#match? @_template_string "([sS][eE][lL][eE][cC][tT].*[fF][rR][oO][mM]|[dD][eE][lL][eE][tT][eE].*[fF][rR][oO][mM]|[iI][nN][sS][eE][rR][tT].*[iI][nN][tT][oO]|[uU][pP][dD][aA][tT][eE].*([sS][eE][tT]|[fF][rR][oO][mM]))")) @sql
- (#offset! @sql 0 1 0 -1))
+(template_string (string_fragment) @injection.content
+  (#match? @injection.content "([sS][eE][lL][eE][cC][tT].*[fF][rR][oO][mM]|[dD][eE][lL][eE][tT][eE].*[fF][rR][oO][mM]|[iI][nN][sS][eE][rR][tT].*[iI][nN][tT][oO]|[uU][pP][dD][aA][tT][eE].*([sS][eE][tT]|[fF][rR][oO][mM]))")
+  (#set! injection.language "sql"))
