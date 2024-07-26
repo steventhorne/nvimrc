4e58592d5865101b1d2d29c8e832fc51e3c1c3ef
steventhorne <steven.thorne91@gmail.com>
2024-07-26 14:55:48 -0400

    Use mini.icons instead of web devicons

--- lua/sthorne/plugins/oil.lua
+++ lua/sthorne/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
+      { "echasnovski/mini.icons", opts = {} },
     },
     config = function()
       require("oil").setup({
