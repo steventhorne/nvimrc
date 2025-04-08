c55a86c661b763f69edbecdd35fa3348680fdfe3
steventhorne <steven.thorne91@gmail.com>
2025-04-08 14:50:53 -0400

    Add more keymaps for git

--- lua/sthorne/plugins/fugitive.lua
+++ lua/sthorne/plugins/fugitive.lua
@@ -6,6 +6,8 @@ return {
       vim.keymap.set("n", "<LEADER>gb", ":Git blame<CR>", { silent = true })
       vim.keymap.set("n", "<LEADER>gd", ":Ghdiffsplit<CR>", { silent = true })
       vim.keymap.set("n", "<LEADER>gl", ":Git log<CR>", { silent = true })
+      vim.keymap.set("n", "<LEADER>gf", ":Git fetch<CR>", { silent = true })
+      vim.keymap.set("n", "<LEADER>gp", ":Git pull --autostash<CR>", { silent = true })
     end,
   },
 }
