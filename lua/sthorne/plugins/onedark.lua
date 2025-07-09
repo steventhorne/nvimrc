06488a05b015c0c66a437a6f4f70211fe78c09ea
steventhorne <steven.thorne91@gmail.com>
2025-07-09 10:29:02 -0400

    Updates to onedark theme

--- lua/sthorne/plugins/onedark.lua
+++ lua/sthorne/plugins/onedark.lua
@@ -1,6 +1,7 @@
 return {
   {
     "navarasu/onedark.nvim",
+    priority = 1000,
     config = function()
       local au_onedark = vim.api.nvim_create_augroup("OneDark", { clear = true })
       vim.api.nvim_create_autocmd("ModeChanged", {
@@ -76,29 +77,6 @@ return {
           undercurl = true,
           background = true,
         },
-
-        -- overrides = function(c)
-        --   return {
-        --     -- general
-        --     Title = { fg = c.red0, style = style.Bold },
-        --     -- modes
-        --     NormalMode = { fg = c.green0, style = style.Bold },
-        --     InsertMode = { fg = c.blue0, style = style.Bold },
-        --     VisualMode = { fg = c.purple0, style = style.Bold },
-        --     CommandMode = { fg = c.red0, style = style.Bold },
-        --     SelectMode = { fg = c.cyan0, style = style.Bold },
-        --     ReplaceMode = { fg = c.red2, style = style.Bold },
-        --     TermMode = { fg = c.green0, style = style.Bold },
-        --     -- quickfix
-        --     qfLineNr = { fg = c.fg_gutter },
-        --     -- nvim-cmp
-        --     PmenuSel = { fg = c.bg1, bg = c.blue0 },
-        --     CmpItemAbbrMatch = { fg = c.blue0, style = style.Bold },
-        --     CmpItemAbbrMatchFuzzy = { link = 'CmpItemAbbrMatch' },
-        --     CmpItemKindFile = { link = 'NvimTreeFolderIcon' },
-        --     CmpItemKindFolder = { link = 'CmpItemKindFile' }
-        --   }
-        -- end
       })
       require("onedark").load()
     end,
